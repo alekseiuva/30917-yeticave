d8eb43b5ec55ebc4be9661ec4d7097d447a9c4da
Alexandra Godun <mister.blblbl@gmail.com>
2017-09-05 02:01:29 +0300

    Render single lot page

--- lot.php
+++ lot.php
@@ -1,11 +1,23 @@
 <?php
 require_once 'functions.php';
+require_once 'data/lots.php';
+require_once 'data/bets.php';
 
-$indexData = [
-    'bets' => $bets,
-];
+if (isset($_GET['id'])) {
+    $lotId = $_GET['id'];
+    $lot = array_key_exists($lotId, $lots) ? $lots[$lotId] : null;
+}
 
-$lotContent = renderTemplate('./templates/lot.php', $indexData);
+if (isset($lot)) {
+    $lotContent = renderTemplate('./templates/lot.php', [
+        'bets' => $bets,
+        'lot' => $lot,
+    ]);
+} else {
+    http_response_code(404);
+    $lotContent = renderTemplate('./templates/404.php', []);
+}
+
 $html = renderTemplate('./templates/layout.php', [
     'content' => $lotContent,
     'title' => 'DC Ply Mens 2016/2017 Snowboard',
---
+++ templates/404.php
@@ -0,0 +1,3 @@
+<section class="container error-page">
+    <h2>Ошибка 404. Страница не найдена.</h2>
+</section>
